3c7bfae1fe38a54c85686105cb7c7b04493a021b
Weihung <weihung@inno-orz.com>
2019-11-25 15:15:20 +0800

    New Version

---
+++ docs/precache-manifest.15265eed36d2454592d1bbc620ad6568.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c66bf9af7f4a2b90a1a5d7e2317b616",
+    "url": "https://weihung.github.io/dice_demo/index.html"
+  },
+  {
+    "revision": "52a9e082d62a824af080",
+    "url": "https://weihung.github.io/dice_demo/static/css/main.ef357fe5.chunk.css"
+  },
+  {
+    "revision": "55e9cd8d17dfc82970c9",
+    "url": "https://weihung.github.io/dice_demo/static/js/2.a09f2922.chunk.js"
+  },
+  {
+    "revision": "52a9e082d62a824af080",
+    "url": "https://weihung.github.io/dice_demo/static/js/main.ecf2c1b6.chunk.js"
+  },
+  {
+    "revision": "174fc030eaa34fc87473",
+    "url": "https://weihung.github.io/dice_demo/static/js/runtime-main.7a6135f0.js"
+  },
+  {
+    "revision": "e75004fdee7d958ff5c9f5c7545dcae8",
+    "url": "https://weihung.github.io/dice_demo/static/media/king.e75004fd.png"
+  },
+  {
+    "revision": "8212d4403499ffd53e4e268a41c69503",
+    "url": "https://weihung.github.io/dice_demo/static/media/king_selected.8212d440.png"
+  },
+  {
+    "revision": "c70e2fd20e2fa3553ce4d84c0df1499e",
+    "url": "https://weihung.github.io/dice_demo/static/media/pound.c70e2fd2.png"
+  },
+  {
+    "revision": "2a22afff17a3ca92fee266673dfd3699",
+    "url": "https://weihung.github.io/dice_demo/static/media/pound_selected.2a22afff.png"
+  }
+]);
