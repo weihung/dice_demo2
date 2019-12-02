1a9e8a087af93c0a36afd3ea95de9eb0c68ba0b0
Weihung <weihung@inno-orz.com>
2019-12-02 20:32:15 +0800

    Update UI

--- docs/precache-manifest.ea079d097adaca13686e4661c2c3890a.js
+++ docs/precache-manifest.ea079d097adaca13686e4661c2c3890a.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11df5bfac0c012dd46ea4686e990f01d",
+    "revision": "6995b138607a876b234012e4250573e4",
     "url": "https://weihung.github.io/dice_demo2/index.html"
   },
   {
-    "revision": "b1085dc539971e17b4c1",
-    "url": "https://weihung.github.io/dice_demo2/static/css/main.ef357fe5.chunk.css"
+    "revision": "448f2c91e19f49b70682",
+    "url": "https://weihung.github.io/dice_demo2/static/css/main.561213c6.chunk.css"
   },
   {
-    "revision": "5748fd6ee614147a1ffa",
-    "url": "https://weihung.github.io/dice_demo2/static/js/2.5948ce1c.chunk.js"
+    "revision": "0a09061f54cc3cea8377",
+    "url": "https://weihung.github.io/dice_demo2/static/js/2.d599bbad.chunk.js"
   },
   {
-    "revision": "b1085dc539971e17b4c1",
-    "url": "https://weihung.github.io/dice_demo2/static/js/main.731dbf27.chunk.js"
+    "revision": "448f2c91e19f49b70682",
+    "url": "https://weihung.github.io/dice_demo2/static/js/main.c2d6ae9d.chunk.js"
   },
   {
     "revision": "6c37938b1e58c76a12ca",
     "url": "https://weihung.github.io/dice_demo2/static/js/runtime-main.39820054.js"
+  },
+  {
+    "revision": "3884620a773b56d84f6eee19f77289d1",
+    "url": "https://weihung.github.io/dice_demo2/static/media/coin_flip.3884620a.png"
   },
   {
     "revision": "e75004fdee7d958ff5c9f5c7545dcae8",
@@ -34,5 +38,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2a22afff17a3ca92fee266673dfd3699",
     "url": "https://weihung.github.io/dice_demo2/static/media/pound_selected.2a22afff.png"
+  },
+  {
+    "revision": "b9c7b57fd500693f8e97aa2bef428c7c",
+    "url": "https://weihung.github.io/dice_demo2/static/media/two_dice.b9c7b57f.png"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://weihung.github.io/dice_demo2/precache-manifest.9e5cd30066be774c2c3ecdb9d53edbf0.js"
+  "https://weihung.github.io/dice_demo2/precache-manifest.ea079d097adaca13686e4661c2c3890a.js"
 );
 
 self.addEventListener('message', (event) => {
